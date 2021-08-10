8a149b47652e1ebd387d030bc38a68ae165615de
zzangoobrother <com1368@naver.com>
2021-08-10 16:50:02 +0900

    스프링 시작

---
+++ src/main/java/com/example/makecore/MakeCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.makecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeCoreApplication.class, args);
+    }
+
+}
