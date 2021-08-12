f1575bb3f08a72eb842aea56a37dbad863c5bfd0
zzangoobrother <com1368@naver.com>
2021-08-12 16:38:55 +0900

    할인 정책 변경 및 구현

---
+++ src/main/java/com/example/makecore/discount/RateDiscountPolicy.java
@@ -0,0 +1,17 @@
+package com.example.makecore.discount;
+
+import com.example.makecore.member.Grade;
+import com.example.makecore.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
--- src/main/java/com/example/makecore/order/OrderServiceImpl.java
+++ src/main/java/com/example/makecore/order/OrderServiceImpl.java
@@ -1,14 +1,14 @@
 package com.example.makecore.order;
 
 import com.example.makecore.discount.DiscountPolicy;
-import com.example.makecore.discount.FixDiscountPolicy;
+import com.example.makecore.discount.RateDiscountPolicy;
 import com.example.makecore.member.Member;
 import com.example.makecore.member.MemberRepository;
 import com.example.makecore.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
