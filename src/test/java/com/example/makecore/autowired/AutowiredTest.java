afc7ccd47b4db78e40021ac584324d9041bb33bf
zzangoobrother <com1368@naver.com>
2021-08-20 19:44:25 +0900

    Autowired 중복 테스트

---
+++ src/test/java/com/example/makecore/autowired/AutowiredTest.java
@@ -0,0 +1,34 @@
+package com.example.makecore.autowired;
+
+import com.example.makecore.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+    @Test
+    void AutowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+        @Autowired(required = false)
+        public void setNoBean1(Member member) {
+            System.out.println("member = " + member);
+        }
+
+        @Autowired
+        public void setNoBean2(@Nullable Member member) {
+            System.out.println("member = " + member);
+        }
+
+        @Autowired
+        public void setNoBean3(Optional<Member> member) {
+            System.out.println("member = " + member);
+        }
+    }
+}
