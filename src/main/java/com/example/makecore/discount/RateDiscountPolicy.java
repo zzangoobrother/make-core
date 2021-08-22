b4a62a59b70a2a2bc12bf91af63d8ec48b5c39bd
zzangoobrother <com1368@naver.com>
2021-08-22 11:43:43 +0900

    애노테이션 만들기 테스트

--- src/main/java/com/example/makecore/discount/RateDiscountPolicy.java
+++ src/main/java/com/example/makecore/discount/RateDiscountPolicy.java
@@ -1,12 +1,13 @@
 package com.example.makecore.discount;
 
+import com.example.makecore.annotation.MainDiscountPolicy;
 import com.example.makecore.member.Grade;
 import com.example.makecore.member.Member;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
     private int discountPercent = 10;
 
--- src/main/java/com/example/makecore/order/OrderServiceImpl.java
+++ src/main/java/com/example/makecore/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.makecore.order;
 
+import com.example.makecore.annotation.MainDiscountPolicy;
 import com.example.makecore.discount.DiscountPolicy;
 import com.example.makecore.member.Member;
 import com.example.makecore.member.MemberRepository;
@@ -7,10 +8,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
