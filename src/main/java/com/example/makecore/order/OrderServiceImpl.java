271919139782bfea8e0e19b62bb72a015270ea92
zzangoobrother <com1368@naver.com>
2021-08-22 11:37:35 +0900

    Bean 생성 중복 @Primary로 우선순위 지정

--- src/main/java/com/example/makecore/order/OrderServiceImpl.java
+++ src/main/java/com/example/makecore/order/OrderServiceImpl.java
@@ -3,19 +3,14 @@ package com.example.makecore.order;
 import com.example.makecore.discount.DiscountPolicy;
 import com.example.makecore.member.Member;
 import com.example.makecore.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
