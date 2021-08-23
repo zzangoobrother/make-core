59c0cac5e749295b110261cc5538acf94f0b9c43
zzangoobrother <com1368@naver.com>
2021-08-23 12:28:55 +0900

    Bean 생명주기 테스트 2.@Bean에서 initethod, destroyMethod 사용

--- src/test/java/com/example/makecore/lifecycle/NetworkClient.java
+++ src/test/java/com/example/makecore/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package com.example.makecore.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -29,14 +26,12 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() {
         disconnect();
     }
 }
